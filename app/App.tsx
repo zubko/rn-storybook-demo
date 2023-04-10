aabb6a81d38536611b9e63a9435902ee166d6c7f
Alexander Zubko <alexander.zubko@gmail.com>
2023-04-10 22:40:08 +0200

    fix(Home): move HomeNavigator to home feature

--- app/App.tsx
+++ app/App.tsx
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { HomeNavigator } from "./core/navigation/HomeStack";
-import { useAuthStoreToken } from "./features/auth/store/authStore";
-import { AuthNavigator } from "./features/auth/navigation/AuthNavigator";
+import { HomeNavigator } from "@app/features/home/navigation/HomeNavigator";
+import { useAuthStoreToken } from "@app/features/auth/store/authStore";
+import { AuthNavigator } from "@app/features/auth/navigation/AuthNavigator";
 
 export const App = () => {
   const token = useAuthStoreToken();
