ea68d46de90644bbcffdf18e61033c03a5a718db
Alexander Zubko <alexander.zubko@gmail.com>
2023-04-13 23:07:49 +0200

    chore: cleanup file

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
-import { AppRegistry, NativeModules } from "react-native";
+import { AppRegistry } from "react-native";
 import { name as appName } from "./app.json";
 import { App } from "./app/App";
 
 AppRegistry.registerComponent(appName, () => App);
-
-if (__DEV__) {
-  const QAStorybookTriggerModule = NativeModules.QAStorybookTriggerModule;
-  // QAStorybookTriggerModule.initialize();
-}
